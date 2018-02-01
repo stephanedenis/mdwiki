8a1ea286b1160227856bba69c712ce9dd17fe6b2
JoGauvin <jonathangauvin27@gmail.com>
2018-02-01 09:58:02 -0500

    Set back to csharp
    Changed back to csharp by default

--- js/gimmicks/prism.js
+++ js/gimmicks/prism.js
@@ -28,7 +28,7 @@
         var trigger = params.trigger;
         var text = params.text;
 
-        domElement.addClass("language-python");
+        domElement.addClass("language-csharp");
         done();
     };
 
