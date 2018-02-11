505f7e1c1b1688b1213cb9c96cab72f8cc3ce075
Nicolas Lapointe <nixcocpp@gmail.com>
2018-02-11 13:40:54 -0500

    (Re-)IMplemented the Theme(Chooser) gimmick

--- js/ts/theme.ts
+++ js/ts/theme.ts
@@ -72,7 +72,7 @@ module MDwiki.Core {
 
         private applyTheme (theme: Theme): void {
 
-            $('link[rel=stylesheet][href*="netdna.bootstrapcdn.com"]').remove();
+            $('link[rel=stylesheet][href*="bootstrapcdn.com"]').remove();
             var link_tag = this.createLinkTag(theme.styles[0]);
             $('head').append(link_tag);
         }
@@ -81,10 +81,10 @@ module MDwiki.Core {
             return $('<link rel="stylesheet" type="text/css">').attr('href', url);
         }
     }
-/*
-    export class ThemeChooserGimmick extends Gimmick {
+
+    export class ThemeChooserGimmick extends Gimmick.Gimmick {
         constructor() {
-            super();
+            super('ThemeChooser');
             var tc = new ThemeChooser ();
             registerDefaultThemes(tc);
 
@@ -93,30 +93,41 @@ module MDwiki.Core {
                 done();
             });
 
-            var build_chooser = ($links, opt, text) => {
+            var build_chooser = (params: Gimmick.LinkGimmickReference, done: Function) => {
                 tc.enableChooser = true;
-                themechooser($links, opt, text, tc);
+                themechooser(
+                    params.domElement, 
+                    params.options,
+                    params.text,
+                    tc);
+                done();
             };
-            var apply_theme = ($links, opt, text) => {
-                set_theme($links, opt, text, tc);
+            var apply_theme = (params: Gimmick.SinglelineGimmickReference, done: Function) => {
+                set_theme(
+                    params.domElement,
+                    params.options,
+                    params.text,
+                    tc
+                );
+                done();
             }
 
-            this.addHandler('themechooser', build_chooser, 'skel_ready');
-            this.addHandler('theme', apply_theme);
-
+            this.addHandler(new Gimmick.GimmickHandler('link', build_chooser));
+            this.addHandler(new Gimmick.GimmickHandler('singleline', apply_theme));
         }
     };
-*/
-    var set_theme = function($links, opt, text, tc: ThemeChooser) {
+
+    var set_theme = function($elem: JQuery, opt: any, text: string, tc: ThemeChooser) {
         opt.name = opt.name || text;
-        $links.each(function (i, link) {
-            $.md.stage('postgimmick').subscribe(function(done) {
-                if (!tc.currentTheme ||  tc.currentTheme == '' || tc.enableChooser == false)
-                    tc.load(opt.name);
-                done();
-            });
+
+        $.md.wiki.stages.getStage('postgimmick').subscribe(function(done) {
+            if (!tc.currentTheme || tc.currentTheme == '' || !tc.enableChooser) {
+                tc.load(opt.name);
+            }
+            done();
         });
-        $links.remove();
+
+        $elem.remove();
     };
 
     function registerDefaultThemes(tc: ThemeChooser) {
@@ -128,45 +139,38 @@ module MDwiki.Core {
     }
 
     // creates the "Select Theme" navbar entry
-    var themechooser = function($links, opt, text, tc: ThemeChooser) {
-        return $links.each(function(i, e) {
-            var $this = $(e);
-            var $chooser = $('<a href=""></a><ul></ul>'
-            );
-            $chooser.eq(0).text(text);
+    var themechooser = function($elem: JQuery, opt: any, text: string, tc: ThemeChooser) {
+        var $chooser = $('<a href=""></a><ul></ul>');
+        $chooser.eq(0).text(text);
 
-            $.each(tc.themeNames, function(i: number, themeName: string) {
-                var $li = $('<li></li>');
-                $chooser.eq(1).append($li);
-                var $a = $('<a/>')
-                    .text(themeName)
-                    .attr('href', '')
-                    .click(function(ev) {
-                        ev.preventDefault();
-                        tc.currentTheme = themeName;
-                        window.location.reload();
-                    })
-                    .appendTo($li);
-            });
-
-            $chooser.eq(1).append('<li class="divider" />');
-            var $li = $('<li/>');
-            var $a_use_default = $('<a>Use default</a>');
-            $a_use_default.click(function(ev) {
-                ev.preventDefault();
-                tc.currentTheme = '';
-                window.location.reload();
-            });
-            $li.append($a_use_default);
+        $.each(tc.themeNames, function(i: number, themeName: string) {
+            var $li = $('<li></li>');
             $chooser.eq(1).append($li);
 
-            $chooser.eq(1).append('<li class="divider" />');
-            $chooser.eq(1).append('<li><a href="http://www.bootswatch.com">Powered by Bootswatch</a></li>');
-            $this.replaceWith($chooser);
+            var $a = $('<a/>')
+                .text(themeName)
+                .attr('href', '')
+                .click(function(ev) {
+                    ev.preventDefault();
+                    tc.currentTheme = themeName;
+                    window.location.reload();
+                })
+                .appendTo($li);
         });
+
+        $chooser.eq(1).append('<li class="divider" />');
+        var $li = $('<li/>');
+        var $a_use_default = $('<a>Use default</a>');
+        $a_use_default.click(function(ev) {
+            ev.preventDefault();
+            tc.currentTheme = '';
+            window.location.reload();
+        });
+        $li.append($a_use_default);
+        $chooser.eq(1).append($li);
+
+        $chooser.eq(1).append('<li class="divider" />');
+        $chooser.eq(1).append('<li><a href="http://www.bootswatch.com">Powered by Bootswatch</a></li>');
+        $elem.replaceWith($chooser);
     };
 }
-
-
-
-
